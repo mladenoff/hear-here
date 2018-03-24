6dc514792479c4b9e7dbd5e14eee63ee36883446
Isak Mladenoff <imladenoff@gmail.com>
2018-03-23 19:26:31 -0700

    Create root component

--- store/index.js
+++ store/index.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import thunk from 'redux-thunk';
 
-import rootReducer from '../reducers/root_reducer';
+import rootReducer from '../reducers/rootReducer';
 
 const middlewares = [
   thunk,
