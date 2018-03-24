70ea71530fdde358b7668b8f94f1ddb42a08a86f
Isak Mladenoff <imladenoff@gmail.com>
2018-03-24 16:41:34 -0700

    Begin implementing top-level reducers

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux';
 import songs from './songsReducer';
 import bands from './bandsReducer';
-import setlists from './setlistsReducer';
-
-import songs from './songsReducer';
-import bands from './bandsReducer';
 import session from './sessionReducer';
 import setlists from './setlistsReducer';
 
