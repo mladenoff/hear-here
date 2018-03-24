c1b754af0f8f167d4923f8c4bf3e55fdc7fe0e29
Isak Mladenoff <imladenoff@gmail.com>
2018-03-24 15:14:05 -0700

    Create Main componenet

--- components/Root.jsx
+++ components/Root.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+
 import Nav from './nav/Nav';
+import Main from './Main';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
       <Nav />
+      <Main />
     </HashRouter>
   </Provider>
 );
