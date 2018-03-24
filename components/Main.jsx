11e965f255c930ff319e013efd34c4fbf984125b
Isak Mladenoff <imladenoff@gmail.com>
2018-03-24 15:14:05 -0700

    Create Main componenet

---
+++ components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Main;
