51b8cf1ec943609b907bcdaee8730d5844fe8083
Isak Mladenoff <imladenoff@gmail.com>
2018-03-24 15:14:05 -0700

    Create Main componenet

--- components/Root.jsx
+++ components/Root.jsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
+<<<<<<< HEAD
 import Dashboard from './Dashboard/Dashboard';
 import VideoFrameContainer from './VideoFrame/VideoFrameContainer';
+=======
+>>>>>>> Create Main componenet
 import Nav from './nav/Nav';
+import Main from './Main';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
+<<<<<<< HEAD
       <div>
         <Dashboard />
         <VideoFrameContainer />
         <Nav />
       </div>
+=======
+      <Nav />
+      <Main />
+>>>>>>> Create Main componenet
     </HashRouter>
   </Provider>
 );
