b898a212ac3b2ed3bce94f024442fbcbe866b77c
Jesse Wong <jnwong17@gmail.com>
2018-03-24 14:46:54 -0700

    committing to rebase from master

--- components/Root.jsx
+++ components/Root.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import App from './App';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <div>
-      Hello world.
+      <App />
     </div>
   </Provider>
 );
---
+++ components/tester.jsx
@@ -0,0 +1,147 @@
+
+// (optional) add server code here
+// Handling all of our errors here by alerting them
+// function handleError(error) {
+//   if (error) {
+//     alert(error.message);
+//   }
+// }
+//
+// function initializeSession() {
+//   const session = OT.initSession(apiKey, sessionId);
+//
+//   // Subscribe to a newly created stream
+//
+//   // Create a publisher
+//   const publisher = OT.initPublisher('publisher', {
+//     insertMode: 'append',
+//     width: '100%',
+//     height: '100%'
+//   }, handleError);
+//
+//   // Connect to the session
+//   session.connect(token, function(error) {
+//     // If the connection is successful, publish to the session
+//     if (error) {
+//       handleError(error);
+//     } else {
+//       session.publish(publisher, handleError);
+//     }
+//   });
+// }
+
+
+import React from 'react';
+import { OTSession, OTPublisher, OTStreams, OTSubscriber } from 'opentok-react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      connection: 'Connecting',
+      publishVideo: true,
+    };
+
+    this.sessionEventHandlers = {
+      sessionConnected: () => {
+        this.setState({ connection: 'Connected' });
+      },
+      sessionDisconnected: () => {
+        this.setState({ connection: 'Disconnected' });
+      },
+      sessionReconnected: () => {
+        this.setState({ connection: 'Reconnected' });
+      },
+      sessionReconnecting: () => {
+        this.setState({ connection: 'Reconnecting' });
+      },
+    };
+
+    this.publisherEventHandlers = {
+      accessDenied: () => {
+        console.log('User denied access to media source');
+      },
+      streamCreated: () => {
+        console.log('Publisher stream created');
+      },
+      streamDestroyed: ({ reason }) => {
+        console.log(`Publisher stream destroyed because: ${reason}`);
+      },
+    };
+
+    this.subscriberEventHandlers = {
+      videoEnabled: () => {
+        console.log('Subscriber video enabled');
+      },
+      videoDisabled: () => {
+        console.log('Subscriber video disabled');
+      },
+    };
+  }
+
+  onSessionError = error => {
+    this.setState({ error });
+  };
+
+  onPublish = () => {
+    console.log('Publish Success');
+  };
+
+  onPublishError = error => {
+    this.setState({ error });
+  };
+
+  onSubscribe = () => {
+    console.log('Subscribe Success');
+  };
+
+  onSubscribeError = error => {
+    this.setState({ error });
+  };
+
+  toggleVideo = () => {
+    this.setState({ publishVideo: !this.state.publishVideo });
+  };
+
+  render() {
+    const { apiKey, sessionId, token } = this.props.credentials;
+    const { error, connection, publishVideo } = this.state;
+    return (
+      <div>
+        <div id="sessionStatus">Session Status: {connection}</div>
+        {error ? (
+          <div className="error">
+            <strong>Error:</strong> {error}
+          </div>
+        ) : null}
+        <OTSession
+          apiKey={apiKey}
+          sessionId={sessionId}
+          token={token}
+          onError={this.onSessionError}
+          eventHandlers={this.sessionEventHandlers}
+        >
+          <button id="videoButton" onClick={this.toggleVideo}>
+            {publishVideo ? 'Disable' : 'Enable'} Video
+          </button>
+          <OTPublisher
+            properties={{ publishVideo, width: 50, height: 50, }}
+            onPublish={this.onPublish}
+            onError={this.onPublishError}
+            eventHandlers={this.publisherEventHandlers}
+          />
+          <OTStreams>
+            <OTSubscriber
+              properties={{ width: 100, height: 100 }}
+              onSubscribe={this.onSubscribe}
+              onError={this.onSubscribeError}
+              eventHandlers={this.subscriberEventHandlers}
+            />
+          </OTStreams>
+        </OTSession>
+      </div>
+    );
+  }
+}
