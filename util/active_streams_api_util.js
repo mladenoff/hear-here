f09d22e4ab6628b814b62bd824190fcaadad9314
AJ Ansel <Andrew.ansel6@gmail.com>
2018-03-25 01:05:08 -0700

    Create active_streams_api_util

---
+++ util/active_streams_api_util.js
@@ -0,0 +1,183 @@
+let url = "https://data.blockbusting65.hasura-app.io/v1/query/";
+
+export const createActiveStream = (setlistId, sessionId) => {
+  let requestOptions = {
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+    },
+    "credentials": "include"
+  };
+
+  let body = {
+    "type": "insert",
+    "args": {
+      "table": "active_streams",
+      "objects": [
+        {
+          "setlist_id": setlistId,
+          "session_id": sessionId
+        }
+      ],
+      "returning": [
+        "id",
+        "setlist_id",
+        "session_id"
+      ]
+    }
+  };
+
+  requestOptions.body = JSON.stringify(body);
+
+  return fetch(url, requestOptions)
+    .then(function (response) {
+      return response.json();
+    })
+    .catch(function (error) {
+      console.log('Request Failed:' + error);
+    });
+};
+
+export const fetchActiveStream = (activeStreamId) => {
+  let requestOptions = {
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "credentials": "include"
+  };
+
+  let body = {
+    "type": "select",
+    "args": {
+      "table": "active_streams",
+      "columns": [
+        "id",
+        "setlist_id",
+        "session_id"
+      ],
+      "where": {
+        "id": {
+          "$eq": activeStreamId
+        }
+      }
+    }
+  };
+
+  requestOptions.body = JSON.stringify(body);
+
+  return fetch(url, requestOptions)
+    .then(function (response) {
+      return response.json();
+    })
+    .catch(function (error) {
+      console.log('Request Failed:' + error);
+    });
+};
+
+export const fetchActiveStreamsBySessionId = (sessionId) => {
+  let requestOptions = {
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "credentials": "include"
+  };
+
+  let body = {
+    "type": "select",
+    "args": {
+      "table": "active_streams",
+      "columns": [
+        "id",
+        "setlist_id",
+        "session_id"
+      ],
+      "where": {
+        "session_id": {
+          "$eq": sessionId
+        }
+      }
+    }
+  };
+
+  requestOptions.body = JSON.stringify(body);
+
+  return fetch(url, requestOptions)
+    .then(function (response) {
+      return response.json();
+    })
+    .catch(function (error) {
+      console.log('Request Failed:' + error);
+    });
+};
+
+export const fetchActiveStreamsBySetlistId = (setlistId) => {
+  let requestOptions = {
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "credentials": "include"
+  };
+
+  let body = {
+    "type": "select",
+    "args": {
+      "table": "active_streams",
+      "columns": [
+        "id",
+        "setlist_id",
+        "session_id"
+      ],
+      "where": {
+        "setlist_id": {
+          "$eq": setlistId
+        }
+      }
+    }
+  };
+
+  requestOptions.body = JSON.stringify(body);
+
+  return fetch(url, requestOptions)
+    .then(function (response) {
+      return response.json();
+    })
+    .catch(function (error) {
+      console.log('Request Failed:' + error);
+    });
+};
+
+export const deleteActiveStream = (activeStreamId) => {
+  let requestOptions = {
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "credentials": "include"
+  };
+
+  let body = {
+    "type": "delete",
+    "args": {
+      "table": "active_streams",
+      "where": {
+        "id": {
+          "$eq": activeStreamId
+        }
+      }
+    }
+  };
+
+  requestOptions.body = JSON.stringify(body);
+
+  return fetch(url, requestOptions)
+    .then(function (response) {
+      return response.json();
+    })
+    .catch(function (error) {
+      console.log('Request Failed:' + error);
+    });
+};
+
