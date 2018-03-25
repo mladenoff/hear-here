8d40d5f43b96d264ccf7577065a1ca36103b73b8
niartenyaw <amwayne90@gmail.com>
2018-03-24 17:17:07 -0700

    Add Tabs, BackButton, and Nav Search

--- components/Root.jsx
+++ components/Root.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
 import Dashboard from './Dashboard/Dashboard';
 import VideoFrameContainer from './VideoFrame/VideoFrameContainer';
+import Nav from './nav/Nav';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <div>
-      <Dashboard />
-      <VideoFrameContainer />
-    </div>
+    <HashRouter>
+      <div>
+        <Dashboard />
+        <VideoFrameContainer />
+        <Nav />
+      </div>
+    </HashRouter>
   </Provider>
 );
 
