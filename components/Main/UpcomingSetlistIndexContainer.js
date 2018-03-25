7602e7996c423bea66e17675b0ec4318cf740559
Isak Mladenoff <imladenoff@gmail.com>
2018-03-25 00:43:20 -0700

    Get SetlistIndexItems working

--- components/Main/UpcomingSetlistIndexContainer.js
+++ components/Main/UpcomingSetlistIndexContainer.js
@@ -1,14 +1,19 @@
 import { connect } from 'react-redux';
 
 import SetlistIndex from './SetlistIndex';
-
-const upcomingSelector = (setlists) => {
-  return setlists;
-};
+import { fetchSetlists, setAsPlaying } from '../../actions/setlistActions';
+import { fetchBands } from '../../actions/bandActions';
+import { upcomingSelector } from '../../reducers/selectors';
 
 const mapStateToProps = state => ({
   bands: state.bands,
   setlists: upcomingSelector(state.setlists),
 });
 
-export default connect(mapStateToProps)(SetlistIndex);
+const mapDispatchToProps = dispatch => ({
+  fetchSetlists: () => dispatch(fetchSetlists()),
+  fetchBands: () => dispatch(fetchBands()),
+  setAsPlaying: setlistId => dispatch(setAsPlaying(setlistId)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SetlistIndex);
