1bcfd63eb745f7354e233199cb7765f9f1bed6c4
dwebster17 <dwebster1992@gmail.com>
2018-03-24 19:26:16 -0700

    Remove import statement from main.js

--- components/main.jsx
+++ components/main.jsx
@@ -7,13 +7,6 @@ import configureStore from '../store/index';
 
 import Root from './Root';
 
-import {
-  API_KEY,
-  SESSION_ID,
-  TOKEN,
-  API_SECRET
-} from '../config/config';
-
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
@@ -22,22 +15,3 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('root')
   );
 });
-//
-//
-// if (API_KEY && TOKEN && SESSION_ID && (5 > 10)) {
-//   renderApp({
-//     apiKey: API_KEY,
-//     sessionId: SESSION_ID,
-//     token: TOKEN,
-//   });
-// } else {
-//   const ot = new OpenTok(API_KEY, API_SECRET);
-//
-//   const maybePromise = ot.createSession((err, session) => {
-//     if (err) return console.log(err);
-//
-//     console.log(session.sessionId);
-//   })
-//
-//   debugger
-// }
