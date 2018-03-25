a371e5a0e3c6b79070841cd0b6a648f9342ecb6d
niartenyaw <amwayne90@gmail.com>
2018-03-24 22:42:19 -0700

    Fix tabs on manage page to have bottom border

--- components/nav/Nav.jsx
+++ components/nav/Nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import UserTabsContainer from './Tabs/UserTabsContainer';
 import ManageTabsContainer from './Tabs/ManageTabsContainer';
 import LeftNav from './LeftNav';
 import MenuContainer from './Menu/MenuContainer';
 import NavSearch from './NavSearch';
 
-export default () => (
+const Nav = () => (
   <header>
     <section className="upper-nav">
       <LeftNav />
@@ -20,3 +21,5 @@ export default () => (
     </Switch>
   </header>
 );
+
+export default withRouter(Nav);
