1c3a482b12f7d3470ecc41a361986dcac6815784
niartenyaw <amwayne90@gmail.com>
2018-03-24 20:16:52 -0700

    Change Nav folder to follow convention

--- components/Root.jsx
+++ components/Root.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
-import Nav from './nav/Nav';
+import Nav from './Nav/Nav';
 import Main from './Main';
 
 const Root = ({ store }) => (
