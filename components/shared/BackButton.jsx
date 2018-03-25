e0eb0da7867f2121d7d1018b22f23e551981f04b
niartenyaw <amwayne90@gmail.com>
2018-03-25 12:31:19 -0700

    Change back arrow from search to be an image

--- components/shared/BackButton.jsx
+++ components/shared/BackButton.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { withRouter } from 'react-router';
 
 const BackButton = ({ history: { goBack } }) => (
-  <button onClick={goBack}>{'<-'}</button>
+  <button onClick={goBack}>
+    <img
+      className="back-arrow"
+      src="./assets/small-back-arrow.png" />
+  </button>
 );
 
 export default withRouter(BackButton);
