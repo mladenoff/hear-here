2f860094e6ace3bbf1304beccf8079bbd9be0cd1
AJ Ansel <Andrew.ansel6@gmail.com>
2018-03-25 02:57:13 -0700

    Add session actions and reducers

--- actions/sessionActions.js
+++ actions/sessionActions.js
@@ -1,6 +1,27 @@
+import * as APIUtil from '../util/session_api_util';
+import * as UserAPIUtil from '../util/user_api_util';
+
 export const RECEIVE_USER = 'RECEIVE_USER';
 
 export const receiveUser = user => ({
   type: RECEIVE_USER,
   user,
 });
+
+export const signup = formUser => (dispatch) => {
+  return UserAPIUtil.createUser(formUser).then(
+    user => dispatch(receiveUser(user.returning[0])),
+  );
+};
+
+export const login = formUser => (dispatch) => {
+  return APIUtil.createSession(formUser).then(
+    user => dispatch(receiveUser(user)),
+  );
+};
+
+export const logout = () => (dispatch) => {
+  return APIUtil.deleteSession().then(
+    response => dispatch(receiveUser(null)),
+  );
+};
--- reducers/sessionReducer.js
+++ reducers/sessionReducer.js
@@ -1,6 +1,9 @@
+import { RECEIVE_USER } from "../actions/sessionActions";
+
 const sessionReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
+    case RECEIVE_USER:
     default:
       return state;
   }
