1fc78cb5e0c7326985cc8d8615a2d5cb091ed9b7
AJ Ansel <Andrew.ansel6@gmail.com>
2018-03-25 03:28:09 -0700

    Fix session reducer's case statement

--- reducers/sessionReducer.js
+++ reducers/sessionReducer.js
@@ -1,9 +1,11 @@
 import { RECEIVE_USER } from "../actions/sessionActions";
 
-const sessionReducer = (state = {}, action) => {
+const sessionReducer = (state = { currentUser: null }, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_USER:
+      const currentUser = action.currentUser;
+      return Object.assign({}, { currentUser });
     default:
       return state;
   }
