63cd7acab72bc80194f1997e587d1cdd0e0fcb1e
Isak Mladenoff <imladenoff@gmail.com>
2018-03-24 17:33:41 -0700

    Write setlistActions

--- actions/setlistActions.js
+++ actions/setlistActions.js
@@ -1,6 +1,11 @@
 export const SET_AS_PLAYING = 'SET_AS_PLAYING';
+export const SET_NOT_PLAYING = 'SET_NOT_PLAYING';
 
 export const setAsPlaying = setlistId => ({
   type: SET_AS_PLAYING,
+  setlistId,
+});
+export const setNotPlaying = setlistId => ({
+  type: SET_NOT_PLAYING,
   setlistId,
 });
