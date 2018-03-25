586a9fd822f0724a76445f21ba57e7c8a017a629
Isak Mladenoff <imladenoff@gmail.com>
2018-03-24 19:27:48 -0700

    Factor out nowPlayingSelector

--- components/Main/LiveSetlistIndexContainer.js
+++ components/Main/LiveSetlistIndexContainer.js
@@ -1,18 +1,7 @@
 import { connect } from 'react-redux';
 
 import SetlistIndex from './SetlistIndex';
-
-const nowPlayingSelector = (setlists) => {
-  const filteredLists = {};
-
-  Object.keys(setlists).map((key) => {
-    if (setlists.key.nowPlaying) {
-      filteredLists[key] = setlists.key;
-    }
-  });
-
-  return filteredLists;
-};
+import { nowPlayingSelector } from '../../reducers/selectors';
 
 const mapStateToProps = state => ({
   bands: state.bands,
