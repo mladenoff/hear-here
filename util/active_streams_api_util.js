9472ddcd083f992b050068510b8e4cc63cee8687
AJ Ansel <Andrew.ansel6@gmail.com>
2018-03-25 14:35:07 -0700

    Create fetchAllActiveStreams api util

--- util/active_streams_api_util.js
+++ util/active_streams_api_util.js
@@ -200,6 +200,60 @@ export const fetchActiveStreamsBySessionId = (sessionId) => {
     });
 };
 
+export const fetchAllActiveStreams = () => {
+  let requestOptions = {
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "credentials": "include"
+  };
+
+  let body = {
+    "type": "select",
+    "args": {
+      "table": "active_streams",
+      "columns": [
+        "id",
+        "setlist_id",
+        "session_id",
+        "active_song_id",
+        {
+          "name": "setlist",
+          "columns": [
+            "id",
+            "name",
+            "band_id",
+            "description",
+            "scheduled_date"
+          ]
+        },
+        {
+          "name": "active_song",
+          "columns": [
+            "id",
+            "setlist_id",
+            "file_url",
+            "title",
+            "ord",
+            "archive_id"
+          ]
+        }
+      ]
+    }
+  };
+
+  requestOptions.body = JSON.stringify(body);
+
+  return fetch(url, requestOptions)
+    .then(function (response) {
+      return response.json();
+    })
+    .catch(function (error) {
+      console.log('Request Failed:' + error);
+    });
+};
+
 export const fetchActiveStreamsBySetlistId = (setlistId) => {
   let requestOptions = {
     "method": "POST",
