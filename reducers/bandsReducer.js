8cbd9a939dd5850418723ee7823dd2c1bf72788b
Isak Mladenoff <imladenoff@gmail.com>
2018-03-24 18:32:46 -0700

    Implement first API call to bands table

--- reducers/bandsReducer.js
+++ reducers/bandsReducer.js
@@ -1,6 +1,13 @@
+import { RECEIVE_BANDS } from '../actions/bandActions';
+
 const bandsReducer = (state = {}, action) => {
+  let newState;
   Object.freeze(state);
   switch (action.type) {
+    case RECEIVE_BANDS:
+      newState = {};
+      action.bands.map((band) => { newState[band.id] = band; });
+      return Object.assign({}, state, newState);
     default:
       return state;
   }
