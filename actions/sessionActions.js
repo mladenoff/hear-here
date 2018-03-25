2787a135a1bf76cd068acdfb6407949652cdd806
AJ Ansel <Andrew.ansel6@gmail.com>
2018-03-25 03:29:50 -0700

    Change RECEIVE_USER to RECEIVE_CURRENT_USER

--- actions/sessionActions.js
+++ actions/sessionActions.js
@@ -1,27 +1,27 @@
 import * as APIUtil from '../util/session_api_util';
 import * as UserAPIUtil from '../util/user_api_util';
 
-export const RECEIVE_USER = 'RECEIVE_USER';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 
-export const receiveUser = user => ({
-  type: RECEIVE_USER,
+export const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
   user,
 });
 
 export const signup = formUser => (dispatch) => {
   return UserAPIUtil.createUser(formUser).then(
-    user => dispatch(receiveUser(user.returning[0])),
+    user => dispatch(receiveCurrentUser(user.returning[0])),
   );
 };
 
 export const login = formUser => (dispatch) => {
   return APIUtil.createSession(formUser).then(
-    user => dispatch(receiveUser(user)),
+    user => dispatch(receiveCurrentUser(user)),
   );
 };
 
 export const logout = () => (dispatch) => {
   return APIUtil.deleteSession().then(
-    response => dispatch(receiveUser(null)),
+    response => dispatch(receiveCurrentUser(null)),
   );
 };
--- reducers/sessionReducer.js
+++ reducers/sessionReducer.js
@@ -1,9 +1,9 @@
-import { RECEIVE_USER } from "../actions/sessionActions";
+import { RECEIVE_CURRENT_USER } from "../actions/sessionActions";
 
 const sessionReducer = (state = { currentUser: null }, action) => {
   Object.freeze(state);
   switch (action.type) {
-    case RECEIVE_USER:
+    case RECEIVE_CURRENT_USER:
       const currentUser = action.currentUser;
       return Object.assign({}, { currentUser });
     default:
